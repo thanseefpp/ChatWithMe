11a916049d3f1327488a01daba5a353c84fb85ae
thanseefpp <androtsf@gmail.com>
2023-07-16 23:09:41 +0400

    app started

---
+++ app/app.py
@@ -0,0 +1,13 @@
+#----------------------------------------------- IMPORTING DEPENDENCIES -----------------------------------------#
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.document_loaders import TextLoader
+from langchain.chains import RetrievalQA,  ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
+
+#------------------------------------------------- CONFIGURATION ------------------------------------------------#
